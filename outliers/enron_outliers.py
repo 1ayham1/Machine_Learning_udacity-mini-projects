92f0127a6a8202b9b1523fcd566989297b0d93c3
me <learn.ayham@zoho.com>
2021-09-14 22:59:49 +0200

    simple plot to see outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+"""reads in the data (in dictionary form) and converts it into a sklearn-ready numpy array."""
 
 import joblib
 import sys
@@ -12,8 +12,12 @@ data_dict = joblib.load( open("../final_project/final_project_dataset.pkl", "rb"
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
-### your code below
-
-
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
